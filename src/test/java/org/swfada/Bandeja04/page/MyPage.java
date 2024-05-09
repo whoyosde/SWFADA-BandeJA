6933a6909c0558b008e69ffde86e1e371a4ee4b4
whoyosde-nttdata <109999577+whoyosde@users.noreply.github.com>
2024-05-09 18:18:47 -0500

    Update MyPage.java

--- src/test/java/org/swfada/Bandeja04/page/MyPage.java
+++ src/test/java/org/swfada/Bandeja04/page/MyPage.java
@@ -4,9 +4,13 @@ import net.serenitybdd.core.annotations.findby.FindBy;
 import net.serenitybdd.core.pages.PageObject;
 import net.serenitybdd.core.pages.WebElementFacade;
 import org.openqa.selenium.interactions.Actions;
+
 import static org.junit.Assert.assertTrue;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.util.Arrays;
 import java.util.Comparator;
@@ -17,11 +21,7 @@ import java.io.File;
 import java.util.List;
 
 
-
 public class MyPage extends PageObject {
-
-    @FindBy(xpath = "//table[@id='resultadoComunicacionesRecibidas']/tbody/tr")
-    private List<WebElementFacade> filas;
 
     @FindBy(xpath = "//a[@id=\"descargarZip\"]")
     private WebElementFacade btnDescargarDocumento;
@@ -32,43 +32,43 @@ public class MyPage extends PageObject {
     @FindBy(id = "cerrarModalInfo")
     private WebElementFacade btnCerrar;
 
-    private WebElement filaSeleccionada;
-    private String codigo;
 
     public void usuarioSeleccionaComunicacionLista() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), 20);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.id("resultadoComunicacionesRecibidas_wrapper")));
+        String codigo = "EXT/2024/0000000000001254";
+        // Encontrar todas las filas de la tabla
+        List<WebElement> filas = getDriver().findElements(By.tagName("tr"));
+        // Bandera para indicar si se encontró el elemento
+        boolean encontrado = false;
 
-        int cantReg = filas.size();
+        for (int i = 0; i < filas.size(); i++) {
+            WebElement fila = filas.get(i);
+            // Buscar el elemento en la fila por su texto
+            if (fila.getText().contains(codigo)) {
+                // Encontrar la columna deseada (por ejemplo, la tercera columna)
+                Actions actions = new Actions(getDriver());
+                actions.moveToElement(fila)
+                        .perform();
+                WebElement detalle = fila.findElement(By.xpath("(//tr[" + (i - 1) + "]/td[10]/div[2]/img[2])"));
 
-        if (cantReg > 0) {
-            Random valorAleatorio = new Random();
-            int filaSelccionadaIndex;
-            String codigo;
-
-            for (int i = 0; i < cantReg ; i++) {
-                filaSelccionadaIndex = valorAleatorio.nextInt(cantReg);
-                filaSeleccionada = filas.get(filaSelccionadaIndex);
-                WebElement columnsCodigo = filaSeleccionada.findElement(By.xpath(".//td[5]"));
-                codigo = columnsCodigo.getText();
-
-                String codigoDeseado = "INT/2024/0000000000001335";
-
-                if (codigo.equals(codigoDeseado)) {
-                    this.codigo = codigo;
-                    filaSeleccionada.click();
-                    break;
-                }
+                actions.moveToElement(detalle)
+                        .perform();
+                detalle.click();
+                encontrado = true;
+                break;
             }
         }
+        if (!encontrado) {
+            System.out.println("No se encontró el elemento buscado.");
+        }
+
     }
 
-    public void usuarioPresionaIconoInformacionDetallada() {
-
-        WebElement iconoInformacionDetallada = filaSeleccionada.findElement(By.xpath(".//img[@alt='informacion']"));
-        iconoInformacionDetallada.click();
-    }
 
     public void usuarioVisualizaVentanaInformacionDetallada() {
-
+        WebDriverWait wait = new WebDriverWait(getDriver(), 10);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("#modal > div > div > div.modal-header > h4")));
         WebElement tituloInformacionDetallada = getDriver().findElement(By.cssSelector("#modal > div > div > div.modal-header > h4"));
         assertTrue(tituloInformacionDetallada.getText().contains("Información detallada"));
     }
@@ -94,20 +94,21 @@ public class MyPage extends PageObject {
 
     public void usuarioValidoDocumentoDescargado() {
 
-            String downloadPath = "C:\\Users\\lmanriqp\\Downloads";
-            // Obtener lista de archivos en el directorio de descargas
+        //String downloadPath = "C:\\Users\\lmanriqp\\Downloads";
+        String downloadPath = "C:\\Users\\whoyosde\\Downloads";
+        // Obtener lista de archivos en el directorio de descargas
 
-            File dir = new File(downloadPath);
-            File[] files = dir.listFiles();
-            // Ordenar archivos por fecha de modificación descendente
-            Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
-            // Verificar si se descargó algún archivo
-            if (files.length > 0) {
-                // El archivo más reciente estará en la posición 0 del arreglo
-                File archivoDescargado = files[0];
-                System.out.println("El archivo más reciente descargado es: " + archivoDescargado.getName());
-            } else {
-                System.out.println("No se descargaron archivos.");
-            }
+        File dir = new File(downloadPath);
+        File[] files = dir.listFiles();
+        // Ordenar archivos por fecha de modificación descendente
+        Arrays.sort(files, Comparator.comparingLong(File::lastModified).reversed());
+        // Verificar si se descargó algún archivo
+        if (files.length > 0) {
+            // El archivo más reciente estará en la posición 0 del arreglo
+            File archivoDescargado = files[0];
+            System.out.println("El archivo más reciente descargado es: " + archivoDescargado.getName());
+        } else {
+            System.out.println("No se descargaron archivos.");
+        }
     }
 }
